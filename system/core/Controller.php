b04603e5418888cd01f6bb8b7fed5b86f0b1a00c
yahir <edson.ruvalcaba@alumnos.udg.mx>
2019-03-20 18:42:32 -0600

    add layouts folder

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -10,7 +10,7 @@
 		abstract public function index();
 
 		public function ftwig($path = ''){
-			$loader = new Twig_Loader_Filesystem([ROOT.PROJECT.PATH_VIEWS,ROOT.PROJECT.PATH_VIEWS.$path]);
+			$loader = new Twig_Loader_Filesystem([ROOT.PROJECT.PATH_VIEWS.'layouts/',ROOT.PROJECT.PATH_VIEWS.$path]);
 			$twig = new Twig_Environment($loader,[]);
 			return $twig;
 		}
